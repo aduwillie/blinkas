47025d9d3c12b4222f20fb30a41004581a942e71
Jemima Adjanor <jemimaadjanor@yahoo.com>
2019-05-31 16:22:23 +0100

    create an input component

---
+++ src/frontend/components/Input.test.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import Input from './Input';
+import renderer from 'react-test-renderer'
+
+
+let component;
+
+beforeEach(()=>{
+    component = shallow(<Input/>)
+});
+
+describe('<Input/> rendering', ()=>{
+    it('renders correctly', ()=>{
+        const component = renderer
+        .create(<Input/>)
+        .toJSON();
+            expect(component).toMatchSnapshot();
+    });
+});
+
+ 
