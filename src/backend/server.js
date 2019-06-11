7a4c54e1f7125873ed98efe262273f4a97ccd11e
nicholaschun <nicholaschunrayne@gmail.com>
2019-06-11 12:06:16 -0700

    added semi colons

--- src/backend/server.js
+++ src/backend/server.js
@@ -32,9 +32,9 @@ const init = async (shouldStart) => {
 process.on('unhandledRejection', (err) => {
     console.error(err);
     process.exit(1);
-})
+});
 process.on('uncaughtException', (err) => {
     console.error(err);
     process.exit(1);
-})
+});
 export default init;
