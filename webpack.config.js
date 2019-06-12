b5a8d8064d50d8b14dbfdbde32422e25c38c374a
nicholaschun <nicholaschunrayne@gmail.com>
2019-06-12 13:20:23 -0700

    added style loaders to webpack

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,14 @@ const config = {
     output: {
         path: path.resolve(__dirname, "dist"),
         filename: "bundle.js"
+    },
+    modules: {
+        rules: [
+            {
+                test: /\.(s*)css$/,
+                use: ['style-loader', 'css-loader', 'sass-loader']
+            }
+        ]
     }
 }
 export default config
