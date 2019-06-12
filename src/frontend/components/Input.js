07077391e4e1061f4c49425ba8269cbacfdc7011
Jemima Adjanor <jemimaadjanor@yahoo.com>
2019-06-12 13:08:09 +0100

    used the ideal indentation

--- src/frontend/components/Input.js
+++ src/frontend/components/Input.js
@@ -2,17 +2,16 @@ import React from 'react';
 import inputStyle from './InputStyle'
 
 const Input = (props) => {
-        return (
-                    <input 
-                    style={inputStyle}
-                    type={props.type}
-                    id={props.id}
-                    name={props.name}
-                    value={props.value} 
-                    onChange={props.handleInputChange} 
-                    placeholder={props.placeholder}
-                    />
-        );
-    }
+    return (
+    <input 
+    style={inputStyle}
+    type={props.type}
+    id={props.id}
+    name={props.name}
+    value={props.value}
+    onChange={props.handleInputChange}
+    placeholder={props.placeholder}/>
+    );
+}
 
 export default Input;
--- src/frontend/components/Input.test.js
+++ src/frontend/components/Input.test.js
@@ -2,12 +2,9 @@ import React from 'react';
 import renderer from 'react-test-renderer'
 import Input from './Input';
 
-
 describe('<Input/> rendering', ()=>{
     it('renders correctly', ()=>{
-        const component = renderer
-        .create(<Input/>)
-        .toJSON();
-            expect(component).toMatchSnapshot();
+        const component = renderer.create(<Input/>).toJSON();
+        expect(component).toMatchSnapshot();
     });
 });
