6fb25fb95d9fd21d475bb554be5410eefccf201b
Jemima Adjanor <jemimaadjanor@yahoo.com>
2019-06-12 13:28:16 +0100

    removed newlines

--- src/frontend/components/LoginBanner.js
+++ src/frontend/components/LoginBanner.js
@@ -2,20 +2,15 @@ import React, { Component } from 'react';
 import loginBanner from './loginBanner'
 import Login from './../components/Login'
 
-
 class LoginBanner extends Component {
-    
     render() {
         return (
-         <div style={loginBanner}>
+        <div style={loginBanner}>
             <div style={bannerName}>Blinkas</div>
-            <div>
-                <Login/>
-            </div>
+            <div><Login/></div>
         </div>
         );
     }
-
 }
 
 export default LoginBanner;  
--- src/frontend/components/LoginBanner.test.js
+++ src/frontend/components/LoginBanner.test.js
@@ -2,12 +2,9 @@ import React from 'react'
 import renderer from 'react-test-renderer'
 import LoginBanner from './LoginBanner'
 
-
 describe('<LoginBanner/> rendering', ()=>{
     it('renders correctly', ()=>{
-        const bannerComponent = renderer
-        .create(<LoginBanner/>)
-        .toJSON();
-            expect(bannerComponent).toMatchSnapshot();
+        const bannerComponent = renderer.create(<LoginBanner/>).toJSON();
+        expect(bannerComponent).toMatchSnapshot();
     });
 });
