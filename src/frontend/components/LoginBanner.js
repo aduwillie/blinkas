df97ec2239fee09748b63084b18262255b3dc717
Jemima Adjanor <jemimaadjanor@yahoo.com>
2019-06-18 13:50:31 +0100

    Made changes to thestyle file

--- src/frontend/components/LoginBanner.js
+++ src/frontend/components/LoginBanner.js
@@ -1,12 +1,11 @@
 import React, { Component } from 'react';
-import loginBanner from './loginBanner'
 import Login from './../components/Login'
 
 class LoginBanner extends Component {
     render() {
         return (
-        <div style={loginBanner}>
-            <div style={bannerName}>Blinkas</div>
+        <div className={loginBanner}>
+            <div className={bannerName}>Blinkas</div>
             <div><Login/></div>
         </div>
         );
