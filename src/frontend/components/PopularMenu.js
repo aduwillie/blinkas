3c0344c7b2687d1cbe3f1a63872366f499ac4547
Sayon coulibaly <sayoncoul.sc@gmail.com>
2019-06-18 12:24:02 +0000

    changed popular menus

--- src/frontend/components/PopularMenu.js
+++ src/frontend/components/PopularMenu.js
@@ -1,43 +1,74 @@
 import React, { Component } from 'react'
-import News from './News'
-import Software from './Software'
-import Office from './Office'
-import Reading from './Reading'
-import Video from './Video'
-import Entertainment from './Entertainment'
-import Learning from './Learning'
 import './style.scss'
-
-export default class PopularMenu extends Component {
-    render() {
-        return (
-            <div>
-               <div className="popularMenus" >
-                    <ul >
-                        <li >
-                            <News/>
-                        </li>
-                        <li >
-                            <Software/>
-                        </li>
-                        <li >
-                        <Video/>
-                        </li>                
-                        <li >
-                            <Reading/>
-                        </li>
-                        <li >
-                            <Learning/>                    
-                        </li>
-                        <li >
-                            <Entertainment/>
-                        </li>
-                        <li >
-                            <Office/>
-                        </li>
-                    </ul>
-                </div>
+    
+    
+        const PopularMenus =[
+            {
+                id: 1,
+                title: "News",
+                name:"Organizer",
+                img: "img/signup_news.png",
+                
+              },
+            {
+                id: 2,
+                title: "Software",
+                name:"Organizer",
+                img: "img/signup_software.png",
+                
+              },
+              {
+                id: 3,
+                title: "Video/Movie",
+                name:"Organizer",
+                img: "img/signup_movie_video.png",
+                
+              },
+              {
+                id: 4,
+                title: "Learning",
+                name:"Organizer",
+                img: "img/signup_learning.png",
+                
+              },
+              {
+                id: 5,
+                title: "Reading",
+                name:"Organizer",
+                img: "img/signup_reading.png",
+                
+              },
+              {
+                id: 6,
+                title: "Entertainment",
+                name:"Organizer",
+                img: "img/signup_entertainment.png",
+                
+              },
+              {
+                id: 7,
+                title: "Office & Project",
+                name:"Organizer",
+                img: "img/signup_office_projects.png",
+                
+              },
+        ]
+        const Menus = () => (
+          <div className="popularMenus">
+            <ul>
+              {PopularMenus.map(item => (
+                <li key={item.id}>
+                  <div>
+                  <img src={item.img}  />
+                  </div>
+                  <div>
+                        <span>{item.title}</span><br></br>
+                        <span>{item.name}</span>
+                        </div>
+                  
+                </li>
+              ))}
+            </ul>
             </div>
         )
-    }
-}
+    export default Menus;
