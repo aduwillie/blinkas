a1f0dc2bfd4a5a91b5ea26e6210642d46ac401be
Sayon coulibaly <sayoncoul.sc@gmail.com>
2019-06-19 12:18:34 +0000

    edited select component

--- src/frontend/components/Select.js
+++ src/frontend/components/Select.js
@@ -3,14 +3,14 @@ import './style.scss'
 
 const Select =(props)=> {
    return (
-         <select name ={props.name} className={props.classname} onChange={props.onChange}>
+         <select name ={props.name} className={props.className} onChange={props.onChange}> 
+         <option>{props.defaultOption}</option>        
            {
              props.options.map((item , index)=>(
                <option key ={index} value = {item}>{item}</option>
              ))
            }
-         </select> 
-      
+         </select>       
     )
    
 }
