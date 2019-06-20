29702aaaed5802e433e09da883c95db9d28a8141
jejeadjanor <49441916+jejeadjanor@users.noreply.github.com>
2019-06-20 13:46:00 +0100

    Create Login Banner Component (#16)
    * adding login banner component
    
    * removing extra lines
    
    * removed newlines
    
    * Made changes to thestyle file

---
+++ src/frontend/components/LoginBanner.test.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import renderer from 'react-test-renderer'
+import LoginBanner from './LoginBanner'
+
+describe('<LoginBanner/> rendering', ()=>{
+    it('renders correctly', ()=>{
+        const bannerComponent = renderer.create(<LoginBanner/>).toJSON();
+        expect(bannerComponent).toMatchSnapshot();
+    });
+});
