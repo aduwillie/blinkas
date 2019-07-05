fb9f69597e717a5b39f9703547248bdd1b430365
nicholaschun <nicholaschunrayne@gmail.com>
2019-07-05 12:48:10 -0700

    added css loaders and configured webpack

--- src/frontend/index.js
+++ src/frontend/index.js
@@ -2,5 +2,4 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-
-ReactDOM.render(<h1>Hello welcome to Blinkas homepage</h1>, document.getElementById('root'))
+ReactDOM.render(<h1>Hello world</h1>, document.getElementById('root'));
