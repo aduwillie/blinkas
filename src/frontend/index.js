e24bbf3eb5221048235ffbf2070b181ef55375b6
Moses Basing <mbasing001@st.ug.edu.gh>
2019-07-09 15:36:52 +0100

    created a react landing page (#21)
    * created a react landing page
    
    * imported react_landing_page to index.js and rendered ReactLandingPage component

--- src/frontend/index.js
+++ src/frontend/index.js
@@ -1,5 +1,6 @@
 // console.log('frontend.js'); // eslint-disable-line no-console
 import React from 'react'
 import ReactDOM from 'react-dom'
+import ReactLandingPage from './components/reactLandingPage'
 
-ReactDOM.render(<h1>Hello world</h1>, document.getElementById('root'));
+ReactDOM.render(<ReactLandingPage/>, document.getElementById('root'));
