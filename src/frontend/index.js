d4ca900d21e5250c4bb5b2e4fd832050d136b9d7
nicholaschun <nicholaschunrayne@gmail.com>
2019-07-09 14:42:18 -0700

    renamed react landing page

--- src/frontend/index.js
+++ src/frontend/index.js
@@ -1,6 +1,6 @@
 // console.log('frontend.js'); // eslint-disable-line no-console
 import React from 'react'
 import ReactDOM from 'react-dom'
-import ReactLandingPage from './components/reactLandingPage'
+import ReactLandingPage from './components/ReactLandingPage'
 
 ReactDOM.render(<ReactLandingPage/>, document.getElementById('root'));
