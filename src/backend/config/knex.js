8ea2d80528afed9d16bd91948039afc94ac96f1a
nicholaschun <nicholaschunrayne@gmail.com>
2019-07-10 18:07:15 -0700

    added new line

--- src/backend/config/knex.js
+++ src/backend/config/knex.js
@@ -1,5 +1,5 @@
 import knex from 'knex';
 import knexConfig from '../../../knexfile';
 let environment = process.env.NODE_ENV || 'development';
-let config = knexConfig[environment]
-module.exports = knex(config)
+let config = knexConfig[environment];
+module.exports = knex(config);
